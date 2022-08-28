54581548640e83df23626953c18ceeb564b1ed8d
Oleguzik <guzik.oleg@gmail.com>
2022-08-29 00:02:37 +0200

    Second Task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const resetBtnRef = document.querySelector('#reset-time');
+
+resetBtnRef.addEventListener('click', onResetClick);
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+onPageLoad();
+
+function onTimeupdate(data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+}
+
+function onPageLoad() {
+  const savedVideoTime = localStorage.getItem(CURRENT_TIME);
+  if (savedVideoTime) {
+    player.setCurrentTime(savedVideoTime);
+  }
+}
+
+function onResetClick() {
+  player.pause();
+  player.setCurrentTime(0);
+  localStorage.removeItem(CURRENT_TIME);
+}
